4ae6e81513df6ccdf8589634486a01de9b0a973e
apokrovs <143448415+apokrovs@users.noreply.github.com>
2026-09-20 16:56:58 -0400

    Add: design-word-search-data-structure - submission-8

---
+++ Data Structures & Algorithms/design-word-search-data-structure/submission-8.py
@@ -0,0 +1,38 @@
+class WordDictionary:
+
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def addWord(self, word: str) -> None:
+        current = self.root
+        for c in word:
+            if c not in current.children:
+                current.children[c] = TrieNode()
+            current = current.children[c]
+        current.end = True
+
+    def search(self, word: str) -> bool:
+        current = self.root
+        def dfs(index: int, node: TrieNode) -> bool:
+            if index == len(word):
+                return node.end == True
+            if word[index] != ".":
+                if word[index] in node.children:
+                    return dfs(index+1, node.children[word[index]])
+                else:
+                    return False
+            else:
+                res = False
+                for c in node.children:
+                    if dfs(index+1, node.children[c]):
+                        return True
+                return res
+                    
+        return dfs(0, current)
+
+
+class TrieNode:
+    def __init__(self):
+        self.end = False
+        self.children = {}
+        
